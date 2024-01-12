fbb8c806db954310ee4638af8ed00a594769deab
bellkjtt <100061449+bellkjtt@users.noreply.github.com>
2024-01-12 19:53:08 +0900

    Update 백준 그리디 - 세탁소 사장 동혁 2720번.py

--- 백준/일반/백준 그리디 - 세탁소 사장 동혁 2720번.py
+++ 백준/일반/백준 그리디 - 세탁소 사장 동혁 2720번.py
@@ -20,4 +20,6 @@ for i in range(1,len(ans)+1):
     print(ans[i-1], end=' ')
     if i%4==0:
         print(sep='')
-        
+
+#동전의 갯수가 얼마 안되므로 for 로도 가능하다(if 문사용).
+#만약 동전의 종류가 많아진다면 for for이 더 합리적
